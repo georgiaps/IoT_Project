38a816a8f7db33b03307915dce6fe98c1591bd73
ariadnefyr <ariadne.fyrogeni@gmail.com>
2024-12-02 20:58:13 +0200

    small update

--- code/config.py
+++ code/config.py
@@ -3,5 +3,5 @@ tomtom_API_key = "R51mEIGhkA1ITAySjGZbD5OlRSaJjFHV"
 
 microclimate_sensor_topic = 'Environmental/barani-meteohelix-iot-pro:1' #air temperature, humidity, sun intensity
 wind_sensor_topic = 'json/Environmental/barani-meteowind-iot-pro:1' #wind speed and direction
-noise_sensor_topic = 'Environmental/dutch-sensor-systems-ranos-db-2:1'
+noise_sensor_topic = 'Environmental/dutch-sensor-systems-ranos-db-2:1' #noise
 co2_sensor_topic = 'Room monitoring/mclimate-co2-sensor:1'
